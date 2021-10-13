4b4581de0793eb8cdeb9b1ecc88925d6a7899aae
Anastasia <krayushkinanastya@gmail.com>
2021-10-13 10:38:39 +0200

    Comments

--- Tests/buyTV.js
+++ Tests/buyTV.js
@@ -1,13 +1,32 @@
 const {By, Builder, WebElement} = require("selenium-webdriver");
 require("chromedriver");
 
+// TC steps:
+// go to https://www.alza.cz/
+// Assert logo is present
+// click on element xpath=//*[@href="/tv-foto-audio-video"]
+// Asset that tittle is  TV, foto, audio, video
+// Click on Televize
+// Assert Televize tittle is present
+// Click on Od najdrazsiho
+// Assert Od najdrazsiho tab is active
+// TV_1 = Get text and price of 1st TV
+// Click on Koupit on the 1st TV
+// Click on Koupit bez sluzeb
+// Assert TV added to Busket
+// Go back to search results
+// TV_2 = Get text and price of 2st TV
+// Click on Koupit on the 2nd TV
+// Assert TV is added to Busket
+// Go to Busket and check that TV_1 and TV_2 are there.
+
 async function buyTV() {
     let driver = await new Builder().forBrowser("chrome").build();
     await driver.get("https://www.alza.cz/");
 
     const getLogoAlzaPath = '//div[@id="logo"]';
-    const getTitleTVsection = '//h1[text()="TV, foto, audio, video"]';
-    const getTitleTV = '//h1[text()="Televize"]';
+    const getTitleTVsection = '//h1[@itemprop="name" and text()="TV, foto, audio, video"]';
+    const getTitleTV = '//h1[@itemprop="name" and text()="Televize"]';
     const getTVsSectionPath = '//*[@href="/tv-foto-audio-video"]';
     const getTVsPath = '//*[@href="/televize/18849604.htm"]';
     const getExpenciveTVs = '//a[@id="ui-id-5"]';
@@ -26,19 +45,26 @@ async function buyTV() {
 
     if (logo) {
         console.log('Logo Alza is present');
+    } else {
+        console.log('Logo Alza is not present');
     }
 
     await driver.findElement(By.xpath(getTVsSectionPath)).click();
     if (titleTVsection) {
-        console.log('Title TV, foto, audio, video is present');
+        console.log('Title for TV, foto, audio, video is present');
+    } else {
+        console.log('Title for TV, foto, audio, video is not present');
     }
+
     await driver.findElement(By.xpath(getTVsPath)).click();
     if (titleTV) {
-        console.log('Title TV is present');
+        console.log('Title for TV is present');
+    } else {
+        console.log('Title for TV is not present');
     }
+
     await driver.findElement(By.xpath(getExpenciveTVs)).click();
 
-    await availableTVs[0].click();
     await buyTVBtn.click();
     await backBtn.click();
     await availableTVs[1].click();
@@ -47,4 +73,4 @@ async function buyTV() {
     await driver.quit();
 }
 
-await buyTV();
+buyTV();
